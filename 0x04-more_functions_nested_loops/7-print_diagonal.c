9ae007bfd5a74ae0d2cb66a318883182149d8303
samsonjonathan <sj204259@.com>
2023-09-15 17:39:31 -0700

    Write a function that draws a diagonal line on the terminal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal
+ * @n: no of times characte should be printed
+ */
+void print_diagonal(int n)
+{
+	int gap;
+	int slash;
+
+	if (n > 0)
+	{
+		slash = 1;
+		while (slash <= n)
+		{
+			for (gap = 1; gap < slash; gap++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+			slash++;
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+
+}
